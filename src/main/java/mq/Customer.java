c328c52dedf529407367490ce9f205f088bc2faf
royzhuo <731924600@qq.com>
2018-06-20 10:43:39 +0800

    生产者消费者业务完善

--- src/main/java/mq/Customer.java
+++ src/main/java/mq/Customer.java
@@ -9,18 +9,35 @@ import java.util.Objects;
  * Created by roy.zhuo on 2018/6/14.
  */
 
-
+//消费者
 public class Customer {
 
     public static void main(String[] args) {
+        //1.创建连接工厂
         ConnectionFactory connectionFactory=new ActiveMQConnectionFactory("user","user",ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
         Connection connection=null;
         try {
+            //2..创建连接
             connection=connectionFactory.createConnection();
+            //3.启动连接
             connection.start();
+            //4.创建会话 会话用于创建生产者 消费者 目的地。 不设置事物 ，自动签收
+            /*
+        * 签收？通俗点说，就是消费者接受到消息后，需要告诉消息服务器，我收到消息了。当消息服务器收到回执后，本条消息将失效。因此签收将对PTP模式产生很大影响。
+        * 如果消费者收到消息后，并不签收，那么本条消息继续有效，很可能会被其他消费者消费掉！
+        *
+        * AUTO_ACKNOWLEDGE：表示在消费者receive消息的时候自动的签收
+          CLIENT_ACKNOWLEDGE：表示消费者receive消息后必须手动的调用acknowledge()方法进行签收
+          DUPS_OK_ACKNOWLEDGE：签不签收无所谓了，只要消费者能够容忍重复的消息接受，当然这样会降低Session的开销
+        *CLIENT_ACKNOWLEDGE，采用手动的方式较自动的方式可能更好些，因为接收到了消息，并不意味着成功的处理了消息，假设我们采用手动签收的方式，只有在消息成功处理的前提下才进行签收，那么只要消息处理失败，
+        * 那么消息还有效，仍然会继续消费，直至成功处理！
+        * */
             Session session=connection.createSession(Boolean.FALSE,Session.AUTO_ACKNOWLEDGE);
-            Destination destination=session.createQueue("hello activemQ address");
+            //5.创建目的地
+            Destination destination=session.createQueue("TestQueue");
+            //6.创建消费者
             MessageConsumer messageConsumer=session.createConsumer(destination);
+            //7.创建监听
             messageConsumer.setMessageListener(new MessageListener() {
                 @Override
                 public void onMessage(Message message) {
--- src/main/java/mq/Producer1.java
+++ src/main/java/mq/Producer1.java
@@ -10,26 +10,46 @@ import java.util.Objects;
  * Created by roy.zhuo on 2018/6/14.
  */
 
-
+//生产者
 public class Producer1 {
 
     public static void main(String[] args) {
+        //1.创建连接工厂
         ConnectionFactory connectionFactory= new ActiveMQConnectionFactory("user","user",ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
         Connection connection=null;
         try {
+            //2..创建连接
             connection=connectionFactory.createConnection();
+            //3.启动连接
             connection.start();
+            //4.创建会话 会话用于创建生产者 消费者 目的地。 不设置事物 ，自动签收
             Session session=connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-            Destination destination=session.createQueue("hello activemQ address");
+            //5.创建目的地 目的地有两种模式 队列 和主题
+            /*
+            * 队列：有多个消费者连接队列，队列的消息会平摊给各个消费者
+            * 主题: 主题就是消息的发布和订阅  生产者就是发布消息   消费者订阅消息   消费发布上去会全部推送给各个订阅者
+            *        订阅者如果在消息发布之后订阅，是没有办法接受到消息的。
+            * */
+            Destination destination=session.createQueue("TestQueue");
+            //6.创建生产者
             MessageProducer messageProducer=session.createProducer(destination);
+            //7.不设置持久话
             messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            TextMessage textMessage=new ActiveMQTextMessage();
-            textMessage.setText("hello activeqm qqqqqhhhhh344444666");
-            messageProducer.send(textMessage);
+            for(int i=0;i<100;i++){
+                //8.创建消息体
+                TextMessage textMessage=new ActiveMQTextMessage();
+                textMessage.setText("come on queue "+i);
+                //9.发送消息
+                messageProducer.send(textMessage);
+                System.out.println("发送了: come on queue "+i);
+            }
+            System.out.println("发送成功了");
+
 
         } catch (JMSException e) {
             e.printStackTrace();
         }finally {
+            //10.关闭连接
             if (Objects.nonNull(connection)){
                 try {
                     connection.close();
